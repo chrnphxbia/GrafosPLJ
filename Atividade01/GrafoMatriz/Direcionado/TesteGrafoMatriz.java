6fc3494b9d6bd7d9d3c9ab9c8075e9fff457766f
chrnphxbia <pedrohqfarias@gmail.com>
2024-09-01 19:49:40 -0300

    Adicionando codigo equivalente dos exercicios 1 ao 8

---
+++ Atividade01/GrafoMatriz/Direcionado/TesteGrafoMatriz.java
@@ -0,0 +1,15 @@
+public class TesteGrafoMatriz {
+    public static void main(String args[]) {
+        // TGrafo grafo01 = new TGrafo(4);
+        // grafo01.insereA(0, 1);
+        // grafo01.insereA(0, 2);
+        // grafo01.insereA(1, 3);
+        // grafo01.insereA(2, 1);
+        // grafo01.insereA(2, 3);
+        // grafo01.show();
+        // System.out.println("Grafo é simétrico? " + grafo01.isSimetrico());
+
+        TGrafo grafo02 = new TGrafo("exemplo.txt");
+        grafo02.show();
+    }
+}
---
+++ Atividade01/GrafoMatriz/NaoDirecionado/TGrafoND.java
@@ -0,0 +1,84 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TGrafoND {
+    private	int n; // quantidade de vértices
+	private	int m; // quantidade de arestas
+	private	Double adj[][]; //matriz de adjacência
+
+    public TGrafoND(int n) {  // construtor
+	    this.n = n;
+	    // No início dos tempos não há arestas
+	    this.m = 0; 
+	    // alocação da matriz do TGrafo
+	    this.adj = new Double [n][n];
+
+	    // Inicia a matriz com zeros
+		for(int i = 0; i< n; i++)
+			for(int j = 0; j< n; j++)
+				this.adj[i][j] = Double.POSITIVE_INFINITY;	
+	}
+
+    public TGrafoND(String arquivo) {
+		try {
+			int origem, destino;
+			Double peso;
+
+			Scanner scanner = new Scanner(new File(arquivo));
+			this.n = scanner.nextInt();
+			int linhas = scanner.nextInt();
+
+			this.adj = new Double[this.n][this.n];
+			
+			for(int i = 0; i< n; i++) {
+				for(int j = 0; j< n; j++) {
+					this.adj[i][j] = Double.POSITIVE_INFINITY;
+				}
+			}
+
+			for(int k = 0; k < linhas; k++) {
+				origem = scanner.nextInt();
+				destino = scanner.nextInt();
+				peso = Double.parseDouble(scanner.nextLine());
+				insereA(origem, destino, peso);
+			}
+
+			scanner.close();
+
+		} catch (FileNotFoundException e) {
+			System.err.println("Arquivo não encontrado.");
+		}
+	}
+
+    public void insereA(int v, int w, double peso) {
+	    // testa se nao temos a aresta
+	    if(adj[v][w] == Double.POSITIVE_INFINITY){
+	        adj[v][w] = peso;
+            adj[w][v] = peso;
+	        m++; // atualiza qtd arestas
+	    }
+	}
+
+    public void removeA(int v, int w) {
+	    // testa se temos a aresta
+	    if(adj[v][w] != Double.POSITIVE_INFINITY ){
+	        adj[v][w] = Double.POSITIVE_INFINITY;
+            adj[w][v] = Double.POSITIVE_INFINITY;
+	        m--; // atualiza qtd arestas
+	    }
+	}
+
+    public void show() {
+	    System.out.println("n: " + n );
+	    System.out.println("m: " + m );
+	    for(int i=0; i < n; i++) {
+	    	System.out.print("\n");
+	        for(int w=0; w < n; w++)
+	            if(adj[i][w] != Double.POSITIVE_INFINITY)
+	            	System.out.print("Adj[" + i + "," + w + "]= " + adj[i][w] + " ");
+	            else System.out.print("Adj[" + i + "," + w + "]= " + Double.POSITIVE_INFINITY + " ");
+	    }
+	    System.out.println("\n\nfim da impressao do grafo." );
+	}
+}
